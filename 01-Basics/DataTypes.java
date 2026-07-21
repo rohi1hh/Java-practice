3b472fdf6cd37edc623e61162993134085410c44
rohi1hh <rohithn619@gmail.com>
2026-07-21 22:43:52 +0530

    Add DataTypes class with basic variable examples

---
+++ 01-Basics/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 2000;
+        int age = 18;
+        long mobile = 9876543210L;
+
+        float cgpa = 8.5f;
+        double percentage = 92.75;
+
+        char grade = 'A';
+
+        boolean pass = true;
+
+        String name = "Rohith";
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Age: " + age);
+        System.out.println("Mobile: " + mobile);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Pass: " + pass);
+        System.out.println("Name: " + name);
+    }
+}
