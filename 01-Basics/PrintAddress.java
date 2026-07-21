02dd84b544c086c1665fb361269f74718632f008
rohi1hh <rohithn619@gmail.com>
2026-07-21 22:24:57 +0530

    Add PrintAddress.java to display address details

---
+++ 01-Basics/PrintAddress.java
@@ -0,0 +1,9 @@
+public class Address{
+  public static void main(String[] args){
+    System.out.println("Name: Rohith");
+     System.out.println("House no: 1-23");
+     System.out.println("Mandal:Narsapur");
+     System.out.println("District:Medak");
+     System.out.println("State: Telangana");
+  }
+}
