eb14a0b1abbb3b207254639fe1fe985ab3fca60d
rohi1hh <rohithn619@gmail.com>
2026-07-22 20:25:07 +0530

    Add CompoundInterest class to calculate interest

---
+++ 01-Basics/CompoundIntrest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Principal Amount: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = input.nextDouble();
+
+        System.out.print("Enter Time (in years): ");
+        double time = input.nextDouble();
+
+        double amount = principal * Math.pow((1 + rate / 100), time);
+
+        double compoundInterest = amount - principal;
+
+        System.out.println("Compound Interest = " + compoundInterest);
+        System.out.println("Total Amount = " + amount);
+    }
+}
