e20dd6eab0607a88d20c6f02808b0b0591055209
rohi1hh <rohithn619@gmail.com>
2026-07-22 22:24:48 +0530

    Add ASCIIValue class to display ASCII values

---
+++ 01-Basics/ASCIIValue.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ASCIIValue {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
+
+        int ascii = ch;
+
+        System.out.println("ASCII Value = " + ascii);
+    }
+}
