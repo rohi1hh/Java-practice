9a28379b0472c0a9cc41ec623b10590661024504
rohi1hh <rohithn619@gmail.com>
2026-07-24 23:34:22 +0530

    Add PositiveNegative class to check number sign

---
+++ 02-ConditionalStatements/PositiveNegative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is Positive");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+
+    }
+}
