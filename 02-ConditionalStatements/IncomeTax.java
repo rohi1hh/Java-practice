96643d2afc8dc6f23be2e323cbf034176a068c5c
rohi1hh <rohithn619@gmail.com>
2026-07-29 19:04:05 +0530

    Implement Income Tax calculation in IncomeTax.java

---
+++ 02-ConditionalStatements/IncomeTax.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class IncomeTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Annual Income: ");
+        double income = sc.nextDouble();
+
+        double tax;
+
+        if (income <= 250000) {
+            tax = 0;
+        } else if (income <= 500000) {
+            tax = (income - 250000) * 0.05;
+        } else if (income <= 1000000) {
+            tax = (250000 * 0.05) + ((income - 500000) * 0.20);
+        } else {
+            tax = (250000 * 0.05) + (500000 * 0.20) + ((income - 1000000) * 0.30);
+        }
+
+        System.out.println("Income Tax = " + tax);
+
+        sc.close();
+    }
+}
