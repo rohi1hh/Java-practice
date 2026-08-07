e1a4f309cb62df04e693103280c53a9fb869e23b
rohi1hh <rohithn619@gmail.com>
2026-08-07 10:34:10 +0530

    Add PrimeNumberInRange.java to find prime numbers

---
+++ 03-Loops/PrimeNumberInRange.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int i = start; i <= end; i++) {
+            if (i < 2) {
+                continue;
+            }
+
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
