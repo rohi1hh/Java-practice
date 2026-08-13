b365bf0831c520a3fed3ee92400eb91b5e352a8b
rohi1hh <rohithn619@gmail.com>
2026-08-13 22:31:23 +0530

    Add SumOfEvenNumbers.java to calculate even sum

---
+++ 03-Loops/SumOfEvenNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (2 * i);
+        }
+
+        System.out.println(sum);
+    }
+}
