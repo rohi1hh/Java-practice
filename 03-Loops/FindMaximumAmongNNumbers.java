72cc9e19ad5a0a2a681d09641760ff5df3eb9fdd
rohi1hh <rohithn619@gmail.com>
2026-09-22 20:27:38 +0530

    Add FindMaximumAmongNNumbers.java to find max value

---
+++ 03-Loops/FindMaximumAmongNNumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class FindMaximumAmongNNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
